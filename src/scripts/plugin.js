6db4af9f148001956c0cd30dd0a128aaa6214a97
Anton <samusenkoanton@yandex.ru>
2019-05-05 18:42:03 +0300

    add toggles

--- src/scripts/plugin.js
+++ src/scripts/plugin.js
@@ -2,5 +2,6 @@ import "./easy-piechart.js";
 import "./percentages.js";
 import "./ripple.js";
 import "./boxmessage.js";
+import "./sliders.js";
 import "../styles/main.scss";
 console.log('all files loaded');
